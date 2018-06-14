de2dbe50724aae9dccf2b053fd0a79d233d445b6
Anupam jain <anupam.jain@ranosys.com>
2018-06-14 17:01:08 +0530

    change branch name

--- DappToken/Dapp_Token_Sale/deployfrontend.sh
+++ DappToken/Dapp_Token_Sale/deployfrontend.sh
@@ -2,4 +2,4 @@ rsync -r src/ docs/
 rsync build/contracts/* docs/ 
 git add .
 git commit -m "compile pages of github pages"
-git push origin2 Decenteralize_token_sell_application
+git push origin2 master
