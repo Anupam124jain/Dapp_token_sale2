f5c4c69ce79ccb46b69de0ee27cc5f59ba82605b
Anupam jain <anupam.jain@ranosys.com>
2018-06-15 11:28:22 +0530

    change network configrution

--- truffle.js
+++ truffle.js
@@ -8,7 +8,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     test:{
-    	host:"127.0.0.1",
+    	host:"https://anupam124jain.github.io/",
     	port:8545,
     	network_id:4,
     	gas:4700000
