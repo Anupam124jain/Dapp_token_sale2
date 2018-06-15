f51712548e584eaee8c48eed4bb6d4e1e445ce04
Anupam jain <anupam.jain@ranosys.com>
2018-06-15 11:44:36 +0530

    compile pages of github pages

--- docs/js/app.js
+++ docs/js/app.js
@@ -19,7 +19,7 @@ App = {
       web3 = new Web3(web3.currentProvider);
     } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('https://anupam124jain.github.io/');
+      App.web3Provider = new Web3.providers.HttpProvider('127.0.0.1');
       web3 = new Web3(App.web3Provider);
     }
     return App.initContracts();
